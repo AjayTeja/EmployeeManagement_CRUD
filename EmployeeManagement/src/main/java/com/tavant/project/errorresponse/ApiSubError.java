59c17a68faaeb5e4c15640d86391113f8afbe80d
AjayTeja <43804100+AjayTeja@users.noreply.github.com>
2021-02-13 18:58:49 +0530

    Delete EmployeeManagement/src/main/java/com/tavant/project directory

--- EmployeeManagement/src/main/java/com/tavant/project/errorresponse/ApiSubError.java
+++
@@ -1,9 +0,0 @@
-package com.tavant.project.errorresponse;
-
-import java.io.Serializable;
-
-public abstract class ApiSubError implements Serializable {
-
-	
-	private static final long serialVersionID=1L;
-}
